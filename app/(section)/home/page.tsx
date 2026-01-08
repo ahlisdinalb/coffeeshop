88c37bfe03510153ed9d633f189f76a5a0e19d86
ahlisdinalb <ahlisdinalb@gmail.com>
2026-01-08 19:01:34 +0700

    fix

--- app/(section)/home/page.tsx
+++ app/(section)/home/page.tsx
@@ -5,6 +5,7 @@ import Hero from "@/component/home/hero";
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-[#f5eee6] text-[#24160f]">
+      <Hero />
     </main>
   );
 }
